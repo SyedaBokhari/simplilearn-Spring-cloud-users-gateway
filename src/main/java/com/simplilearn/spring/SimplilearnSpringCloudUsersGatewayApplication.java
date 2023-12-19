3128f2ca7b727675d7e8b60ba087271cc7612c83
unknown <noshaymaano@gmail.com>
2023-12-18 17:17:18 -0700

    First commit

---
+++ src/main/java/com/simplilearn/spring/SimplilearnSpringCloudUsersGatewayApplication.java
@@ -0,0 +1,14 @@
+package com.simplilearn.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class SimplilearnSpringCloudUsersGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplilearnSpringCloudUsersGatewayApplication.class, args);
+	}
+
+}
